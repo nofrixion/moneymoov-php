190ae4a56a9316f20f9383978baa9028b5ab1f73
James Bramich <james.bramich@gmail.com>
2023-03-30 20:10:20 +1100

    added Webhook model

---
+++ src/Client/WebhookClient.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoFrixion\Client;
+
+use NoFrixion\Client\AbstractClient;
+use NoFrixion\Model\Webhook;
+use \RuntimeException;
+
+/**
+ * Webhook: provides convenient PHP access to the MoneyMoov API 'webhook' endpoints.
+ */
+class WebhookClient extends AbstractClient
+{
+
+    /**
+     * @see https://docs.nofrixion.com/reference/post_api-v1-webhooks
+     * @param Webhook $webhook
+     * 
+     * @return array associative array of JSON response
+     */
+    public function createWebhook(Webhook $webhook): array
+    {
+        $url = $this->getApiUrl() . 'webhooks';
+        $headers = $this->getRequestHeaders();
+        $method = 'POST';
+
+        $body = http_build_query([
+            'MerchantID' => $webhook->merchantId,
+            'Type' => $webhook->type,
+            'DestinationUrl' => $webhook->destinationUrl,
+            'Retry' => $webhook->retry ? 'true' : 'false',
+            'Secret' => $webhook->secret,
+            'IsActive' => $webhook->isActive ? 'true' : 'false',
+            'EmailAddress' => $webhook->emailAddress ?? ''
+        ]);
+        $response = $this->getHttpClient()->request($method, $url, $headers, $body);
+
+        if (in_array($response->getStatus(), [200, 201], true)) {
+            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+            //return $response->getBody();
+        } else {
+            throw $this->getExceptionByStatusCode($method, $url, $response);
+        }
+    }
+
+    /**
+     * @see https://docs.nofrixion.com/reference/get_api-v1-webhooks-merchantid
+     * @todo convert return values to an array of NoFrixion\Model\Webhook
+     */
+    public function getWebhooks(string $merchantId): array
+    {
+        $url = $this->getApiUrl() . 'webhooks/' . $merchantId;
+        $headers = $this->getRequestHeaders();
+        $method = 'GET';
+
+        $response = $this->getHttpClient()->request($method, $url, $headers);
+
+        if ($response->getStatus() === 200) {
+            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+        } else {
+            throw $this->getExceptionByStatusCode($method, $url, $response);
+        }
+    }
+
+    /**
+     * @see https://docs.nofrixion.com/reference/put_api-v1-webhooks-id
+     */
+    public function updateWebhook(Webhook $webhook): array
+    {
+        if (!is_null($webhook->id)) {
+            $url = $this->getApiUrl() . 'webhooks/' . $webhook->id;
+        } else {
+            throw new RuntimeException('"null" $id specified. $id is required for updateWebhook().');
+        }
+        $headers = $this->getRequestHeaders();
+        $method = 'PUT';
+
+        $body = http_build_query([
+            'MerchantID' => $webhook->merchantId,
+            'Type' => $webhook->type,
+            'DestinationUrl' => $webhook->destinationUrl,
+            'Retry' => $webhook->retry ? 'true' : 'false',
+            'Secret' => $webhook->secret,
+            'IsActive' => $webhook->isActive ? 'true' : 'false',
+            'EmailAddress' => $webhook->emailAddress ?? ''
+        ]);
+
+        $response = $this->getHttpClient()->request($method, $url, $headers, $body);
+        if (in_array($response->getStatus(), [200, 201], true)) {
+            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
+            //return $response->getBody();
+        } else {
+            throw $this->getExceptionByStatusCode($method, $url, $response);
+        }
+    }
+}
+
+//EOF
