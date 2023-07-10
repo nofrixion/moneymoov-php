fdeeae10a18c0ce8aac8c5a51bc40f7f738a7e8c
James Bramich <james.bramich@gmail.com>
2023-07-10 13:40:17 +1000

    updated dependencies

--- src/Exception/ConnectException.php
+++ src/Exception/ConnectException.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Nofrixion\Exception;
 
+use Nofrixion\Exception\NofrixionException;
+
 class ConnectException extends NofrixionException
 {
     public function __construct(string $curlErrorMessage, int $curlErrorCode)
