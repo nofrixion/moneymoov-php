26ea3c0328eebd212aa786282e02ed7b012bc001
James Bramich <james.bramich@gmail.com>
2023-07-10 12:15:07 +1000

    added phpDoc

--- src/Exception/NoFrixionException.php
+++ src/Exception/NoFrixionException.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Nofrixion\Exception;
 
+/**
+ * Summary of NofrixionException
+ */
 class NofrixionException extends \RuntimeException
 {
+    /**
+     * Summary of __construct
+     * @param string $message
+     * @param int $code
+     * @param \Throwable|null $previous
+     */
     public function __construct(string $message, int $code, \Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
